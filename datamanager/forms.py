448cddce42445e0a0680b98c4eae786aa9bc5b94
Yuheng Chang <yuhengc2@illinois.edu>
2019-07-28 16:58:05 -0500

    added area column in the recommandation form

--- datamanager/forms.py
+++ datamanager/forms.py
@@ -35,6 +35,13 @@ RATE = (
     ('5', '5'),
 )
 
+Area = (
+    ('', 'Choose...'),
+    ('South', 'South Quad'),
+    ('Main', 'Main Quad'),
+    ('North', 'North Quad'),
+)
+
 #   Forms for user to filled out. I use the 'crispy_forms' API to beautify the form.
 #   You can google this API to see how it works and use their documentation as reference.
 class UserInfoForm(forms.Form):
@@ -79,15 +86,17 @@ class recommandationForm(forms.Form):
     race = forms.ChoiceField(choices=RACE)
     gender = forms.ChoiceField(choices=GENDER)
     age = forms.ChoiceField(choices=Age)
+    area = forms.ChoiceField(choices=Area)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Row(
-                Column('age', css_class='form-group col-md-6 mb-0'),
-                Column('race', css_class='form-group col-md-4 mb-0'),
-                Column('gender', css_class='form-group col-md-2 mb-0'),
+                Column('age', css_class='form-group col-md-3 mb-0'),
+                Column('race', css_class='form-group col-md-3 mb-0'),
+                Column('gender', css_class='form-group col-md-3 mb-0'),
+                Column('area', css_class='form-group col-md-3 mb-0'),
                 css_class='form-row'
             ),
             Submit('submit', 'Submit')
